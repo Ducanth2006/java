f4ada062fbe8c445529514803073346708e47a65
Ducanth2006 <nicklazada22@gmail.com>
2025-09-26 22:35:47 +0700

    udate

--- 1.2_nhap_gia_tri_tu_ban-phim/nd.java
+++ 1.2_nhap_gia_tri_tu_ban-phim/nd.java
@@ -1,11 +1,12 @@
 import java.util.Scanner ;
 public class nd {
     public static void main(String[] args){
-        Scanner sc =new Scanner(System.in);
+        Scanner nhap=new Scanner(System.in);
+        int a;
+        a=nhap.nextInt();
+        System.out.println("tôi đã print ra là"+a);
+
         
-
-
-
 
     }
 }
