73b50c8779b41b0c7f5869ed1d017ababcb1a267
Ducanth2006 <nicklazada22@gmail.com>
2025-12-17 20:15:15 +0700

    done

--- src/lab4oop/Student.java
+++ src/lab4oop/Student.java
@@ -3,9 +3,14 @@ package lab4oop;
 public class Student {
     //    thuộc tính
     String fullName;
-
     int namSinh;
     int namHienTai;
+    // dùng constructor có tham so
+    public Student(String fullName, int namSinh, int namHienTai){
+        this.fullName=fullName;
+        this.namHienTai=namHienTai;
+        this.namSinh=namSinh;
+    }
 
     //    phương thức
     public void getTuoi() {
@@ -14,8 +19,8 @@ public class Student {
 
     // hàm main
     public static void main(String[] args) {
-        Student st1 = new Student();
-        st1.fullName = "Mr messi";
+        Student st1 = new Student("Mr messi", 2008, 2025);
+        st1.fullName = "Mr ronaldo ";//sẽ ghi đè được nhé
         st1.namHienTai = 2025;
         st1.namSinh = 2008;
         st1.getTuoi();
