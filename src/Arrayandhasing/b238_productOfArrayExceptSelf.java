7fa64ece5014e8e535840ea0e8e948f64b605d29
Duc anh pc <nguyenducanhhn2k6@gmail.com>
2026-03-22 15:38:45 +0700

    update: add a new solution

---
+++ src/Arrayandhasing/b238_productOfArrayExceptSelf.java
@@ -0,0 +1,16 @@
+package Arrayandhasing;
+
+public class b238_productOfArrayExceptSelf {
+    public int[] productExceptSelfSolution(int[] nums) {
+        int k=nums.length;
+        if(k%2==0){
+            for(int i=0;i<k;i++){
+
+
+            }
+        }
+        return new int[]{k};
+
+    }
+
+}
