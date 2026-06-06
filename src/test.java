c5ccea7c4df5bc595974e06d9416961d8a33a9d1
Duc anh pc <nguyenducanhhn2k6@gmail.com>
2026-06-06 15:31:53 +0700

    update from desktop

--- src/test.java
+++ src/test.java
@@ -1,7 +1,14 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
+
 public class test {
     public static void main(String[] args) {
-        System.out.print("oke");
-        System.out.println("NO");
-        System.out.print("ghi");
+        Deque<Integer> deque= new ArrayDeque<>();
+        deque.offer(1);
+        deque.offer(2);
+        System.out.println(deque);
+        System.out.println(deque.poll());
+        System.out.println(deque.offer(3));
+        System.out.println(deque.peek());
     }
 }
