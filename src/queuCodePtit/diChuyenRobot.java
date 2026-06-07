409dfcc02f4a9f0a6b80ce277cbe24aa1613c5af
Duc anh pc <nguyenducanhhn2k6@gmail.com>
2026-06-07 20:10:58 +0700

    robot end queue and stack

--- src/queuCodePtit/diChuyenRobot.java
+++ src/queuCodePtit/diChuyenRobot.java
@@ -1,8 +1,6 @@
 package queuCodePtit;
 
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class diChuyenRobot {
     private static class Pair{
@@ -32,6 +30,34 @@ public class diChuyenRobot {
             this.step=step;
         }
     }
+    private  static void solve(int xA,int yA,int xB,int yB,HashSet<Pair> set,int n){
+        Deque<Node> deque= new ArrayDeque<>();
+        HashSet<Pair> visited=new HashSet<>();
+        deque.add(new Node(xA,yA,0));
+        visited.add(new Pair(xA,yA));
+        int ans=-1;
+        while(!deque.isEmpty()){
+            Node current=deque.poll();
+            int x= current.x;
+            int y= current.y;
+            int step= current.step;
+            if(x==xB&&y==yB){
+                ans=step;
+                break;
+            }
+            for(int i=0;i<8;i++){
+                int nextX=x+ dx[i];// phải cộng x và y vào nhé vì đây chỉ là tăng và giảm thoai .
+                int nextY= y+dy[i];
+                Pair next=new Pair(nextX,nextY);
+                if(set.contains(next)&&!visited.contains(next)){
+                    visited.add(next);
+                    deque.offer(new Node(nextX,nextY,step+1));
+                }
+            }
+        }
+        System.out.println(ans);
+
+    }
     static final int []dx= new int[]{-1,-1,-1,0,0,1,1,1};
     static final int []dy= new int[]{-1,0,1,-1,1,-1,0,1};
     public static void main(String[] args) {
@@ -51,7 +77,9 @@ public class diChuyenRobot {
                 for(int j=y1;j<=y2;j++){
                     set.add(new Pair(x,j));
                 }
+
             }
+            solve(xA,yA,xB,yB,set,n);
 
         }
     }
