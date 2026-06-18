e6b061b12b17454863a706091e5f103a5c4bfea2
Duc anh pc <nguyenducanhhn2k6@gmail.com>
2026-06-18 20:17:00 +0700

    pc commit

---
+++ src/binaryTree/duyetCayNhiPhanTimKiem1.java
@@ -0,0 +1,58 @@
+package binaryTree;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class duyetCayNhiPhanTimKiem1 {
+    static class Node{
+        int val;
+        Node left,right;
+        public Node(int val){
+            this.val=val;
+            this.left=null;
+            this.right=null;
+        }
+    }
+    private static int index;
+    private static Node buildTree(List<Integer> preorder, int min,int max){
+        if(index>=preorder.size()) return null;
+        int val=preorder.get(index);
+        if(val<min||val>max){
+            return null;
+        }
+        index++;
+        Node root= new Node(val);
+        root.left=buildTree(preorder,min,val);
+        root.right=buildTree(preorder,val,max);
+        return root;
+    }
+    private static void dfs(Node root){
+        if(root==null) return ;
+        dfs(root.left);
+        dfs(root.right);
+        System.out.print(root.val+" ");
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        while(t-->0){
+            int n=Integer.parseInt(br.readLine());
+            List<Integer> preorder= new ArrayList<>();
+            StringTokenizer st= new StringTokenizer(br.readLine());
+            for(int i=0;i<n;i++){
+                preorder.add(Integer.parseInt(st.nextToken()));
+            }
+            index=0;
+            Node root= buildTree(preorder,Integer.MIN_VALUE,Integer.MAX_VALUE);
+            // duyệt postOrder thôi
+            dfs(root);
+            System.out.println();
+
+
+        }
+    }
+}
--- src/binaryTree/reviewDuyetCay1.java
+++ src/binaryTree/reviewDuyetCay1.java
@@ -19,16 +19,32 @@ public class reviewDuyetCay1 {
     }
     private static final Map<Integer,Integer> mapIn= new HashMap<>();
     private static int preIndex;
-    private static Node buildTree(int [] preorder,int startIn,int endIn){
-        return new Node(3);
+    private static Node buildTree(int[]preorder,int startIn,int endIn){
+        if(startIn>endIn) {
+            return null;
+        }
+        int valueCur=preorder[preIndex];
+        preIndex++;
+        int index= mapIn.get(valueCur);
+        Node root= new Node(valueCur);
+        root.left=buildTree(preorder,startIn,index-1);
+        root.right=buildTree(preorder,index+1,endIn);
 
+        return root;
+
+    }
+    private static void postorder(Node root){
+        if(root==null) return ;
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.val+" ");
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
         int t=Integer.parseInt(br.readLine());
         while(t-->0){
             mapIn.clear();
-            int preIndex=0;
+            preIndex=0;
             int n=Integer.parseInt(br.readLine());
             int [] inorder= new int[n];
             int [] preorder=new int[n];
@@ -43,6 +59,10 @@ public class reviewDuyetCay1 {
                 preorder[i]=Integer.parseInt(st.nextToken());
             }
             // build tree thôi
+            Node root=buildTree(preorder,0,n-1);
+            postorder(root);
+            System.out.println();
+
         }
     }
 }
